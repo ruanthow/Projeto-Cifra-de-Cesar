29795a81e365931bb9640ff5e5852945d3b04a7c
ruanthow <ruan.thow3@gmail.com>
2022-10-13 23:32:20 -0300

    Adicionando estilo no site

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 var checkBoxBase64 = document.getElementById("base64");
 var checkBoxCifraDeCesar = document.getElementById("cfc");
+var changeColorButtonCesar = document.getElementById("pillRed");
+var changeColorButtonBase64 = document.getElementById("pillBlue");
 var incrementar = document.getElementById('incrementar');
+var labelIncrementar = document.getElementById("labelIncremntar")
+
 const alphabet = ['.','é','ú','ó','á','í','ã','ç','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 var textNoCrypto = document.getElementById("textNoCrypto");
 var textCrypto = document.getElementById("textCrypto");   
 
 incrementar.value = 1;
+changeColorButtonBase64.style.color = "#20ff42"
+labelIncrementar.style.display = "none";
 
 function mostraIncremento() { 
-    incrementar.style.display = "block"; 
+    incrementar.style.display = "block";
+    labelIncrementar.style.display = "block" 
+    changeColorButtonCesar.style.color = "#20ff42"
+    changeColorButtonBase64.style.color = "#fff"
   }
   
 function escondeIncremento() {
     incrementar.style.display = "none";
+    labelIncrementar.style.display = "none";
+    changeColorButtonCesar.style.color = "#fff"
+    changeColorButtonBase64.style.color = "#20ff42"
   }
 
 checkBoxCifraDeCesar.addEventListener('change', mostraIncremento);
@@ -22,27 +35,37 @@ incrementar.addEventListener('change', ()=>{
 
 function Codificar() {
     
-    
-    var arryText = textNoCrypto.value.toLowerCase().split('') 
-    const getLetter = arryText.map((letter)=>{
-    const indexLetter = alphabet.indexOf(letter)
-    const restartIndex = alphabet.indexOf('.');
-    if(indexLetter != -1){
-     var cryptoLetter = indexLetter + parseInt(incrementar.value)
-      if(cryptoLetter >= alphabet.length){
-          cryptoLetter = parseInt(restartIndex) + parseInt(incrementar.value)
-          console.log(cryptoLetter);
-      }  
+    if(checkBoxCifraDeCesar.checked){
+      
+
+      var arryText = textNoCrypto.value.toLowerCase().split('') 
+      const getLetter = arryText.map((letter)=>{
+      const indexLetter = alphabet.indexOf(letter)
+      const restartIndex = alphabet.indexOf('.');
+      if(indexLetter != -1){
+       var cryptoLetter = indexLetter + parseInt(incrementar.value)
+        if(cryptoLetter >= alphabet.length){
+            cryptoLetter = parseInt(restartIndex) + parseInt(incrementar.value)
+            console.log(cryptoLetter);
+        }  
+      }
+      return alphabet[cryptoLetter]
+      })
+     
+      textCrypto.value = getLetter.join('')
     }
-    return alphabet[cryptoLetter]
-    })
+    else{
+      
+      cryptoBase64 = btoa(textNoCrypto.value)
+      textCrypto.value = cryptoBase64
+    }
    
-    textCrypto.value = getLetter.join('')
 }
 
 function Decodificar(){
     
-    var arryText = textNoCrypto.value.toLowerCase().split('') 
+    if(checkBoxCifraDeCesar.checked){
+      var arryText = textNoCrypto.value.toLowerCase().split('') 
     const getLetter = arryText.map((letter)=>{
     const indexLetter = alphabet.indexOf(letter)
     const restartIndex = alphabet.indexOf('.');
@@ -57,4 +80,9 @@ function Decodificar(){
     })
    
     textCrypto.value = getLetter.join('')
+    }
+    else{
+      cryptoBase64 = atob(textNoCrypto.value)
+      textCrypto.value = cryptoBase64
+    }
 }
